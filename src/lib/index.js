6752f7cc5a8980e3094f20fc86b8dd8199c04cd0
Ken Dale <ken@kendaleiv.com>
2016-08-26 08:31:28 -0400

    Update paths

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,23 +5,26 @@ import PostUrlValidator from './post-url-validator';
 import RecompressJpegs from './recompress-jpegs';
 import npmRun from 'npm-run';
 
+const postsGlob = path.join(process.cwd(), '**/_posts/*.+(md|markdown)');
+const _siteFolder = path.join(process.cwd(), '/_site');
+
 console.log(`ritter-jekyll ${require('../package.json').version}`);
 
 console.log('\n--------------------------------------------------\n');
 
 console.log('Validating post urls...\n');
-new PostUrlValidator().validate('_posts/*.md');
+new PostUrlValidator().validate(postsGlob);
 console.log('urls are valid!');
 
 console.log('\n--------------------------------------------------\n');
 
 console.log('Running JPEG recompression...\n');
-new RecompressJpegs().run('_site/**/*.jpg');
+new RecompressJpegs().run(`${_siteFolder}/**/*.jpg`);
 
 console.log('\n--------------------------------------------------\n');
 
 console.log('Running imagemin-cli...\n');
-const imagemin = npmRun.execSync('imagemin _site/images/* -out-dir=_site/images');
+const imagemin = npmRun.execSync(`imagemin ${_siteFolder}/images/* -out-dir=${_siteFolder}/images`);
 console.log(imagemin.toString());
 
 if (process.env.NODE_ENV !== 'production') {
@@ -29,8 +32,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   console.log('Running markdown-proofing...\n');
   const configurationPath = path.resolve(__dirname, './markdown-proofing-configuration.json');
-  const filesGlob = path.join(process.cwd(), '**/_posts/*.+(md|markdown)');
   const markdownProofing = npmRun.execSync(
-    `markdown-proofing -c "${configurationPath}" "${filesGlob}"`);
+    `markdown-proofing -c "${configurationPath}" "${postsGlob}"`);
   console.log(markdownProofing.toString());
 }
